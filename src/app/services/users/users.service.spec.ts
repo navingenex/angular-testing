7ccee9fbad71042a1d83a92765db3ded70ec5c00
navin.kiit <navin.kiit.nk@gmail.com>
2019-01-22 17:49:41 +0530

    testing service

---
+++ src/app/services/users/users.service.spec.ts
@@ -0,0 +1,66 @@
+import { of } from 'rxjs';
+import { TestBed } from '@angular/core/testing';
+
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    });
+    usersService = TestBed.get(UsersService);
+  });
+
+
+  it('should be created', () => {
+    expect(usersService).toBeTruthy();
+  });
+
+  // test for al method
+  describe('all', () => {
+    it('should return a collection of users', () => {
+      const userResponse = [
+        {
+          id: '1',
+          name: 'Jane',
+          role: 'Designer',
+          pokemon: 'Blastoise'
+        },
+        {
+          id: '2',
+          name: 'Bob',
+          role: 'Developer',
+          pokemon: 'Charizard'
+        }
+      ];
+      let response;
+      spyOn(usersService, 'all').and.returnValue(of(userResponse));
+
+      usersService.all().subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(userResponse);
+    });
+  });
+
+  //find one user
+  describe('findOne', () => {
+    it('should return a single user', () => {
+      const userResponse = {
+        id: '2',
+        name: 'Bob',
+        role: 'Developer',
+        pokemon: 'Charizard'
+      };
+      let response;
+      spyOn(usersService, 'findOne').and.returnValue(of(userResponse));
+
+      usersService.findOne('2').subscribe(res => {
+        response = res;
+      });
+      expect(response).toEqual(userResponse);
+    });
+  });
+});
